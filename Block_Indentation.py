3217460bb6ff396333dd91b26e1b357dde1f44b2
YOGEESH K P <yogeesh793@gmail.com>
2024-07-03 10:14:51 +0530

    Add files via upload

---
+++ Block_Indentation.py
@@ -0,0 +1,22 @@
+# Block Indentation
+#1
+def my_function():
+    a=2
+    return a
+print(my_function())
+
+#2
+#if a>b:                # If block starts here
+#    print(a)           # This is part of the if block
+#else:                  # else must be at the same level as if
+#   print(b)           # This line is part of the else block
+
+#if a>b: print(a)
+#else: print(b)
+
+# single statement will not work
+#if x>y:y=x
+#print(y)
+
+def will():
+    pass
